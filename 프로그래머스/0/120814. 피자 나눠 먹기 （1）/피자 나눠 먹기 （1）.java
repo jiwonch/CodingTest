f82e8f31bd774fb9e5778fcc60151556f557e125
JIWON CHOI <87967386+jiwonch@users.noreply.github.com>
2024-05-15 01:06:17 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        int pizza = 7;
+        
+        if (n % 7 != 0) {
+            answer = n / 7 + 1;
+        } else {
+            answer = n / 7;
+        }
+        return answer;
+    }
+}
