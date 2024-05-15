bbf8a65fe39830a2a3c61126140bf3f53bc0f15d
JIWON CHOI <87967386+jiwonch@users.noreply.github.com>
2024-05-16 00:38:26 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int age) {
+    int answer = 0;
+    
+    int date = 2022;
+    
+    answer = date - age +1;
+    return answer;
+}
