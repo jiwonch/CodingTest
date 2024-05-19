57c649fb00cf2b292963ace1fee7da391dd478ea
JIWON CHOI <87967386+jiwonch@users.noreply.github.com>
2024-05-19 21:31:43 +0900

    [level 0] Title: 가위 바위 보, Time: 2.27 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        
+        for (int i = 0; i < rsp.length(); i++) {
+            char num = rsp.charAt(i);
+            if (num == '2') {
+                answer += "0";
+            } else if (num == '0') {
+                answer += "5";
+            } else if (num == '5') {
+                answer += "2";
+            }
+        }
+        return answer;
+    }
+}
