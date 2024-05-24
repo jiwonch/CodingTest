200b7e5d7f01c6067d67f7810744b664a41063f0
JIWON CHOI <87967386+jiwonch@users.noreply.github.com>
2024-05-25 01:03:22 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,19 @@
+/**
+* @brief 배열 numbers의 원소를 차례로 더하다가 n보다 값이 커지는 순간에 그 값을 리턴
+* @param1 Integer array numbers
+* @param2 Integer n
+* @return Ingerger answer
+*/
+
+function solution(numbers, n) {
+    var answer = 0;
+    
+    for (i = 0; i < numbers.length; i++) {
+        answer += numbers[i];
+        if (answer > n) {
+            break;
+        }
+    }
+    
+    return answer;
+}
