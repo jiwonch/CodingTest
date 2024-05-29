b6449e2f6c88d33fe2a72b7c3a0e9ab5dd964118
JIWON CHOI <87967386+jiwonch@users.noreply.github.com>
2024-05-30 00:13:41 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -0,0 +1,16 @@
+/**
+* @brief 문자열 my_string의 앞의 n 글자로 이루어진 문자열을 반환하는 함수
+* @param1 String my_string
+* @param2 Integer n
+* @return String answer
+*/
+
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        
+        answer = my_string.substring(0, n);
+        
+        return answer;
+    }
+}
