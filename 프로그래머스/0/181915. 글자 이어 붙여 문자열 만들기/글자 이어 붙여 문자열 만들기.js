5d659cd2e6cdbbb124aff4643e41b996496f2711
JIWON CHOI <87967386+jiwonch@users.noreply.github.com>
2024-05-30 18:29:14 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -0,0 +1,15 @@
+/**
+* @brief my_string의 index_list의 원소들에 해당하는 인덱스의 글자들을 순서대로 이어 붙인 문자열을 리턴하는 함수
+* @param1
+* @param2
+* @return
+*/
+
+function solution(my_string, index_list) {
+    var answer = '';
+    
+    for (i = 0; i < index_list.length; i++) {
+        answer += my_string.charAt(index_list[i]);
+    }
+    return answer;
+}
