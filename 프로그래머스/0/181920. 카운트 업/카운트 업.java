1d488dd96d697c1cacc922e20e023e8dbd8fd90c
JIWON CHOI <87967386+jiwonch@users.noreply.github.com>
2024-05-30 18:36:14 +0900

    [level 0] Title: 카운트 업, Time: 0.03 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,21 @@
+/**
+* @brief start_num부터 end_num까지의 숫자를 차례로 담은 리스트를 리턴하는 함수
+* @param1 Integer start_num
+* @param2 Integer end_num
+* @return Integer Array answer
+*/
+
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[end_num - start_num + 1];
+        
+        var num = 0;
+    
+        for (int i = start_num; i <= end_num; i++) {
+            answer[num] = i;
+            num++;
+        }
+        
+        return answer;
+    }
+}
