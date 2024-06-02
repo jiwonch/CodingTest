470a4641ee08777a8ca72b8adf888dc6cb6a4359
JIWON CHOI <87967386+jiwonch@users.noreply.github.com>
2024-06-02 22:47:27 +0900

    [Bronze V] Title: 꼬마 정민, Time: 216 ms, Memory: 17796 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long A = sc.nextLong();
+//        sc.nextLine();
+        long B = sc.nextLong();
+//        sc.nextLine();
+        long C = sc.nextLong();
+        sc.nextLine();
+        sc.close();
+
+        System.out.printf("%d", A+B+C);
+    }
+}
